2f8a87370a80488c4cb8f41b0049e7c86f3eacf1
murisKamaa <engpaulkamau@gmail.com>
2023-10-22 11:20:03 +0300

    Added export defaults

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import '../App.css';
 
-<footer>
-    <div className="footer-content">
-        <p>&copy; 2023 National Productivity and Competitiveness Centre</p>
-        <p>Contact: productivitycentre@labour.go.ke</p>
-    </div>
-</footer>
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>&copy; 2023 National Productivity and Competitiveness Centre</p>
+                <p>Contact: productivitycentre@labour.go.ke</p>
+            </div>
+        </footer>
+    )
+}
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,16 @@
 import '../App.css';
 
-<main>
-    <section className="main-section">
-        <div className="holo">
-            <div className="holo-text">
-                <h2>Welcome to NPCC</h2>
-                <p>Productivity Everywhere.</p>
-            </div>
-        </div>
-    </section>
-</main>
+export default function MainContent() {
+    return (
+        <main>
+            <section className="main-section">
+                <div className="holo">
+                    <div className="holo-text">
+                        <h2>Welcome to NPCC</h2>
+                        <p>Productivity Everywhere.</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
