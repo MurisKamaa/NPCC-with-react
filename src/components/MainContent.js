81ad148754a1c4d59e92a38d0aa0d9a591f878c7
murisKamaa <engpaulkamau@gmail.com>
2023-10-22 13:12:02 +0300

    edits

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,12 +3,10 @@ import '../App.css';
 export default function MainContent() {
     return (
         <main>
-            <section className="main-section">
-                <div className="holo">
-                    <div className="holo-text">
-                        <h2>Welcome to NPCC</h2>
-                        <p>Productivity Everywhere.</p>
-                    </div>
+            <section className="holo">
+                <div className="holo-text">
+                    <h2>Welcome to NPCC</h2>
+                    <p>Productivity Everywhere.</p>
                 </div>
             </section>
         </main>
