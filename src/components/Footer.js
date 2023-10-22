501cd89f87482a66fb278458c5b3bdee72ec5a17
murisKamaa <engpaulkamau@gmail.com>
2023-10-22 11:14:37 +0300

    Added footer and main content

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import '../App.css';
+
+<footer>
+    <div className="footer-content">
+        <p>&copy; 2023 National Productivity and Competitiveness Centre</p>
+        <p>Contact: productivitycentre@labour.go.ke</p>
+    </div>
+</footer>
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+<main>
+    <section className="main-section">
+        <div className="holo">
+            <div className="holo-text">
+                <h2>Welcome to NPCC</h2>
+                <p>Productivity Everywhere.</p>
+            </div>
+        </div>
+    </section>
+</main>
